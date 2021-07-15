46b83422b9277a05fad05facad1431a708214063
Samir Musaev <tp050965@mail.apu.edu.my>
2021-07-16 00:56:49 +0800

    fixed todo list

--- src/App.js
+++ src/App.js
@@ -1,67 +1,63 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { API } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
-import { useEffect, useState } from 'react';
-import { API } from 'aws-amplify';
 
-const initialFormState = {name :'', description: ''}
+const initialFormState = { name: '', description: '' }
 
 function App() {
-
   const [notes, setNotes] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
 
-  useEffect(()=> {
+  useEffect(() => {
     fetchNotes();
   }, []);
 
   async function fetchNotes() {
-    const apiData = await API.graphql({query:listNotes});
+    const apiData = await API.graphql({ query: listNotes });
     setNotes(apiData.data.listNotes.items);
   }
 
   async function createNote() {
     if (!formData.name || !formData.description) return;
-    await API.graphql({ query: createNoteMutation, variables: { input: formData }});
-    setNotes([...notes, formData]);
+    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    setNotes([ ...notes, formData ]);
     setFormData(initialFormState);
   }
 
-  async function deleteNote({id}){
-    const newNotesArray = notes.filter(note=>note.id!==id);
+  async function deleteNote({ id }) {
+    const newNotesArray = notes.filter(note => note.id !== id);
     setNotes(newNotesArray);
-    await API.graphql({query:deleteNoteMutation, variables: {input: {id}}});
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
   }
 
   return (
     <div className="App">
-        <h1> waTodo App</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <input
-          onChange={e => setFormData({...formData, 'description':e.target.value})}
-          placeholder="Note name"
-          value={formData.name}
-        />
-        <input
-          onChange={e => setFormData({... formData, 'description':e.target.value})}
-          placeholder="Note description"
-          value={formData.description}
-        />
-
-        <button onClick={createNote}>Create note</button>
-        <div style={{marginBottom: 30}}>
-          {
-            notes.map(note => (
-              <div key={note.id || note.name}>
-                <h2>{note.name}</h2>
-                <p>{note.description}</p> 
-                <button onClick={()=> deleteNote(note)}>Delete note</button>
-              </div>
-            ))
-          }
-        </div>
+      <h1>My Notes App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Note name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Note description"
+        value={formData.description}
+      />
+      <button onClick={createNote}>Create Note</button>
+      <div style={{marginBottom: 30}}>
+        {
+          notes.map(note => (
+            <div key={note.id || note.name}>
+              <h2>{note.name}</h2>
+              <p>{note.description}</p>
+              <button onClick={() => deleteNote(note)}>Delete note</button>
+            </div>
+          ))
+        }
+      </div>
       <AmplifySignOut />
     </div>
   );
