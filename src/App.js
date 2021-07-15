1a191ab37400d4c6a6543154d8773ba7a4bd03dd
Samir Musaev <tp050965@mail.apu.edu.my>
2021-07-16 00:34:15 +0800

    Todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { listNotes } from './graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+import { useEffect, useState } from 'react';
+import { API } from 'aws-amplify';
+
+const initialFormState = {name :'', description: ''}
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(()=> {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const apiData = await API.graphql({query:listNotes});
+    setNotes(apiData.data.listNotes.items);
+  }
+
+  async function createNote() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createNoteMutation, variables: { input: formData }});
+    setNotes([...notes, formData]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteNote({id}){
+    const newNotesArray = notes.filter(note=>note.id!==id);
+    setNotes(newNotesArray);
+    await API.graphql({query:deleteNoteMutation, variables: {input: {id}}});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+        <h1> waTodo App</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input
+          onChange={e => setFormData({...formData, 'description':e.target.value})}
+          placeholder="Note name"
+          value={formData.name}
+        />
+        <input
+          onChange={e => setFormData({... formData, 'description':e.target.value})}
+          placeholder="Note description"
+          value={formData.description}
+        />
+
+        <button onClick={createNote}>Create note</button>
+        <div style={{marginBottom: 30}}>
+          {
+            notes.map(note => (
+              <div key={note.id || note.name}>
+                <h2>{note.name}</h2>
+                <p>{note.description}</p> 
+                <button onClick={()=> deleteNote(note)}>Delete note</button>
+              </div>
+            ))
+          }
+        </div>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
