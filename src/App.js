6a71ede8b2e867dca3d1ea0ee73cdf97c744bf11
Samir Musaev <tp050965@mail.apu.edu.my>
2021-07-21 00:45:05 +0800

    Added sample event consts

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import './App.css';
 
 import Amplify, { API, graphqlOperation, input} from 'aws-amplify';
 import {createUser, createTeam, createUserTeam, createEvent} from "./graphql/mutations";
+import { updateUser, updateTeam, updateUserTeam, updateEvent } from './graphql/mutations';
 import { listUsers, listTeams, listUserTeams, listEvents } from './graphql/queries';
 import { getUser,  getTeam, getUserTeam, getEvent} from './graphql/queries';
 import awsconfig from "./aws-exports";
@@ -33,23 +34,65 @@ const users = [
 	{name: "Oliver", 		age: 46, gender: "Male", 	skills: [], 											attendanceScore: 100}
 ];
 
-let retrieved = [];
+const events = [
+	{
+		title: "Foodbank Friday @ Gombak", 
+		organizer: "New Hope House", 
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non nisl in nisl vestibulum hendrerit. In ac odio et mi fringilla rutrum. Nam in urna non lacus ultrices volutpat sed nec neque. Donec feugiat viverra risus, ut tempus diam condimentum non. Suspendisse vel ante id lacus commodo laoreet.", 	
+		date: "02-12-2022", 	
+		time:"10:00am-15:30pm"
+	},
+	{
+		title: "Open Doggo Day", 
+		organizer: "New Hope House", 
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non nisl in nisl vestibulum hendrerit. In ac odio et mi fringilla rutrum. Nam in urna non lacus ultrices volutpat sed nec neque. Donec feugiat viverra risus, ut tempus diam condimentum non. Suspendisse vel ante id lacus commodo laoreet.", 	
+		date: "04-11-2022", 	
+		time:"13:00pm-18:00pm"
+	},
+	{
+		title: "KL Street Feeding", 
+		organizer: "New Hope House", 
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non nisl in nisl vestibulum hendrerit. In ac odio et mi fringilla rutrum. Nam in urna non lacus ultrices volutpat sed nec neque. Donec feugiat viverra risus, ut tempus diam condimentum non. Suspendisse vel ante id lacus commodo laoreet.", 	
+		date: "02-12-2022", 	
+		time:"10:00am-15:30pm"
+	},
+];
+
 
 async function fetchUsers(){
+	let retrieved = [];	
 	retrieved = await API.graphql(graphqlOperation(listUsers));
+	await API.graphql(graphqlOperation(updateUser, 
+		{
+			input:
+			{
+				id: retrieved.data.listUsers.items[0].id,
+				gender: "Female"
+			}
+		}
+	));
+
+	console.log(
+		retrieved.data.listUsers.items[0].id, 
+		retrieved.data.listUsers.items[0].gender);
 }
-	
-	//for (var i=0; i<users.length; i++){
-		//await API.graphql(graphqlOperation(createUser, {input:users[i]}));
-	//}
 
+/* temporary. remove comments, and duplicate entries get inserted
+(async function (){
+	for (var i=0; i<events.length; i++){
+		await API.graphql(graphqlOperation(createEvent, {input:events[i]}));
+	}
+})();
 
-//console.log("Helo", retrieved);
+(async function(){
+	for (var i=0; i<users.length; i++){
+		await API.graphql(graphqlOperation(createUser, {input:users[i]}));
+	}
+})();
+*/
+
 
 const App = () => {
-	console.log("We here");
-	fetchUsers().then(()=>console.log(retrieved.data.listUsers.items[0].id));
-	console.log("We here");
 	return (
 		<div>
 			<SideNav />
